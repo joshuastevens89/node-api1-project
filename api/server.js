acd288587ebe23ef94ce5010f2d35a533a0faab3
Joshua Stevens <e2855845@student.dcccd.edu>
2024-12-19 05:53:51 -0600

    finished delete function

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const User = require('./users/model')
 
+
 const server = express()
+server.use(express.json())
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -17,10 +19,15 @@ server.get('/api/users', (req, res) => {
     })
 })
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
     
     User.findById(req.params.id)   
-    .then(users => {
+    .then(user => {
+        if (!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist",
+            })
+        }
         res.json(user)
 
     })
@@ -31,6 +38,51 @@ server.get('/api/users', (req, res) => {
             stack: err.stack,
         })
     })
+})
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for user"
+        })
+    } else {
+        User.insert(user)
+        .then(createdUser => {
+            res.status(201).json(createdUser)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error creating user',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+
+    }
+    
+})
+server.delete('/api/users/:id', async (req, res) => {
+try {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist"
+        })
+    } else {
+        const deletedUser = await User.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+} catch (err) {
+    res.status(500).json({
+        message: 'error deleting user',
+        err: err.message,
+        stack: err.stack,
+    })
+
+}
+
+
 })
 
 server.use('*', (req, res) => {
